f67f3caa86584d71686b03d33b815dd8aaa2bd8c
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-02-19 11:18:05 +0100

    added groundwork for RB Trees

--- src/com/arainko/redblacktrees/RedBlackTree.kt
+++ src/com/arainko/redblacktrees/RedBlackTree.kt
@@ -1,21 +1,41 @@
 package com.arainko.redblacktrees
 
 
-import com.arainko.redblacktrees.helpers.NodeField
-import com.arainko.redblacktrees.helpers.NodeField.*
+import com.arainko.redblacktrees.helpers.TreeNode
+import com.arainko.redblacktrees.helpers.TreeNode.*
 
-class RedBlackTree() {
+class RedBlackTree {
 
-    lateinit var root: NodeField
+    var root: TreeNode = Empty
 
-    constructor(rootNode: Node) : this() {
-        root = TreeNode(rootNode)
-    }
+    fun bstInsert(node: TreeNode) {
+        tailrec fun insert(parent: Node, node: Node): Unit = when {
+            node > parent -> if (parent.right is Empty) {
+                node.parent = parent
+                parent.right = node
+            } else insert(parent.right as Node, node)
 
-    fun bstInsert(node: Node) {
-        if (root is TreeNode) {
-            (root as TreeNode).node
+            else -> if (parent.left is Empty) {
+                node.parent = parent
+                parent.left = node
+            } else insert(parent.left as Node, node)
         }
+        if (root is Empty) root = node else insert(root as Node, node as Node)
     }
 
+}
+
+fun main() {
+    val tree = RedBlackTree()
+    val node1 = Node(1)
+    val node2 = Node(2)
+    val node3 = Node(0)
+    tree.bstInsert(node1)
+    tree.bstInsert(node2)
+    tree.bstInsert(node3)
+
+    println(tree.root)
+    println(tree.root.right)
+    println(tree.root.left)
+    println(tree.root.left.parent)
 }
--- src/com/arainko/redblacktrees/helpers/NodeField.kt
+++
@@ -1,8 +0,0 @@
-package com.arainko.redblacktrees.helpers
-
-import com.arainko.redblacktrees.Node
-
-sealed class NodeField {
-    class TreeNode(var node: Node) : NodeField()
-    object Empty : NodeField()
-}
