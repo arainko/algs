b4a150b102feb43530648af30cf87bd20d1016a9
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-03-15 19:13:16 +0100

    added lcs, not all subsequences tho

--- src/com/arainko/lcs/LCS.kt
+++ src/com/arainko/lcs/LCS.kt
@@ -1,6 +1,7 @@
 package com.arainko.lcs
 
 typealias ElementArray = Array<Array<LcsElement>>
+typealias LengthIndex = Pair<Int, Int>
 
 inline fun <reified T> arrayOfDim(dimX: Int, dimY: Int, noinline init: (Int) -> T): Array<Array<T>> =
         Array(dimY) { Array(dimX, init) }
@@ -12,41 +13,61 @@ fun lcsLength(verticalString: String, horizontalString: String): ElementArray {
             verticalString.length + 1) { LcsElement() }
 
     for (i in 1 .. verticalString.length)
-        for(j in 1 .. horizontalString.length) {
-            if (verticalString[i - 1] == horizontalString[j - 1]) {
-                elements[i][j].value = elements[i-1][j-1].value + 1
-                elements[i][j].sign = Sign.DIAGONAL
-            } else if (elements[i-1][j] >= elements[i][j-1]) {
-                elements[i][j].value = elements[i-1][j].value
-                elements[i][j].sign = Sign.VERTICAL
-            } else {
-                elements[i][j].value = elements[i][j-1].value
-                elements[i][j].sign = Sign.HORIZONTAL
+        for(j in 1 .. horizontalString.length)
+            when {
+                verticalString[i - 1] == horizontalString[j - 1] -> {
+                    elements[i][j].value = elements[i-1][j-1].value + 1
+                    elements[i][j].sign = Sign.DIAGONAL
+                }
+                elements[i-1][j] >= elements[i][j-1] -> {
+                    elements[i][j].value = elements[i-1][j].value
+                    elements[i][j].sign = Sign.VERTICAL
+                }
+                else -> {
+                    elements[i][j].value = elements[i][j-1].value
+                    elements[i][j].sign = Sign.HORIZONTAL
+                }
             }
-        }
+
     return elements
 }
 
-fun printLcs(verticalString: String, horizontalString: String, lcsArray: ElementArray) {
-    fun helper(verticalString: String, horizontalString: String, lcsArray: ElementArray, i: Int, j: Int) {
-        if (i == 0 || j == 0)
-            return
-
-        if (lcsArray[i][j].sign == Sign.DIAGONAL) {
-            helper(verticalString, horizontalString, lcsArray, i-1, j-1)
-            print(verticalString[i])
+fun maxLengthIndices(lcsArray: ElementArray): ArrayList<LengthIndex> {
+    val output: ArrayList<LengthIndex> = ArrayList()
+    for (i in lcsArray.size - 1 downTo 1) {
+        for (j in lcsArray[0].size - 1 downTo 1) {
+            if (lcsArray[i][j].value == lcsArray[lcsArray.size - 1][lcsArray[0].size - 1].value &&
+                    lcsArray[i][j].sign == Sign.DIAGONAL)
+                output.add(i to j)
         }
-        else if (lcsArray[i][j].sign == Sign.VERTICAL) helper(verticalString, horizontalString, lcsArray, i-1, j)
-        else if (lcsArray[i][j].sign == Sign.HORIZONTAL) helper(verticalString, horizontalString, lcsArray, i, j-1)
     }
-    helper(verticalString, horizontalString, lcsArray, horizontalString.length, verticalString.length)
+    return output
+}
+
+fun printLcs(verticalString: String, horizontalString: String, lcsArray: ElementArray) {
+    val indices = maxLengthIndices(lcsArray)
+    fun printHelper(verticalString: String, horizontalString: String, lcsArray: ElementArray, i: Int, j: Int) {
+        if (i == 0 || j == 0) return
+
+        when (lcsArray[i][j].sign) {
+            Sign.DIAGONAL -> {
+                printHelper(verticalString, horizontalString, lcsArray, i - 1, j - 1)
+                print(verticalString[i - 1])
+            }
+            Sign.VERTICAL -> printHelper(verticalString, horizontalString, lcsArray, i - 1, j)
+            Sign.HORIZONTAL -> printHelper(verticalString, horizontalString, lcsArray, i, j - 1)
+        }
+    }
+    indices.forEach {
+        printHelper(verticalString, horizontalString, lcsArray, it.first, it.second)
+    }
+
 }
 
 
-
 fun main() {
-    val testString1 = "acbcf"
-    val testString2 = "abcdaf"
+    val testString1 = "abcabcaa"
+    val testString2 = "acbacba"
     val cos = lcsLength(testString1, testString2)
     printLcs(testString1, testString2, cos)
 //    cos.forEach { println(it.toList()) }
