4d4371bcd550938eebf4a9798bd1cc1fd38f97a7
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-04-17 23:29:36 +0200

    added groundwork for B-Trees

--- src/com/arainko/btrees/Node.kt
+++ src/com/arainko/btrees/Node.kt
@@ -1,8 +1,10 @@
 package com.arainko.btrees
 
 class Node(val degree: Int = 3) {
-    val keys: ArrayList<Int> = ArrayList()
-    val children: ArrayList<Node> = ArrayList()
+    val keys: ArrayList<Int> = ArrayList(degree*2-1)
+    val children: ArrayList<Node> = ArrayList(degree*2-1)
     val isLeaf: Boolean
         get() = children.isEmpty()
+    val keyCount: Int
+        get() = keys.size
 }
