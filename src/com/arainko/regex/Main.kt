5a152d93297501a9139c1a9627ecc3ec04ea5725
arainko <aleksander.rainko99@gmail.com>
2020-05-16 17:16:32 +0200

    added naive pattern matching

--- src/com/arainko/regex/Main.kt
+++ src/com/arainko/regex/Main.kt
@@ -1,12 +1,33 @@
 package com.arainko.regex
 
-fun String.naive(regex: String) {
-    val strippedRegex = regex.dropWhile { it == ' ' }
-    val strippedText = regex.dropWhile { it == ' ' }
-    var doneFlag = false
-    while
+infix fun String.naive(pattern: String) {
+    val expressionLines = this.lines()
+    val strippedPattern = pattern.filter { it != '\n' }
+    expressionLines.forEachIndexed { index, line ->
+        for (i in 0..line.length-strippedPattern.length)
+            if (pattern == line.slice(i until i+strippedPattern.length))
+                println("Found match at line $index, position $i")
+    }
+}
+
+infix fun String.rabinKarp(pattern: String) {
+    val oddNumber = 27077
+    val alphabetSize = 128
+    var patternHash = 0
+    var expressionHash = 0
+    var h = 1
+//    h = (h * alphabetSize).pow
+    val expressionLines = this.lines()
+    val strippedPattern = pattern.filter { it != '\n' }
+
+    expressionLines.forEachIndexed { index, line ->
+        for (i in 0..line.length-strippedPattern.length)
+            if (pattern == line.slice(i until i+strippedPattern.length))
+                println("Found match at line $index, position $i")
+    }
+
 }
 
 fun main() {
-
+    "abababab" naive "abab"
 }
